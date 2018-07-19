bddcbb608596455c3aec45cdb0953ec3e0e0702d
duffyjp <duffy.jp@gmail.com>
2018-07-19 20:03:52 +0000

    Scripts to install and configure passenger.

---
+++ 01_install_passenger.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# https://www.phusionpassenger.com/library/install/nginx/install/oss/bionic/
+sudo apt-get install -y dirmngr gnupg
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
+sudo apt-get install -y apt-transport-https ca-certificates
+
+
+sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger bionic main > /etc/apt/sources.list.d/passenger.list'
+sudo apt-get update
+
+
+sudo apt-get install -y nginx libnginx-mod-http-passenger
---
+++ 02_passenger_configs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# https://www.phusionpassenger.com/library/install/nginx/install/oss/bionic/
+
+if [ ! -f /etc/nginx/modules-enabled/50-mod-http-passenger.conf ]; then sudo ln -s /usr/share/nginx/modules-available/mod-http-passenger.load /etc/nginx/modules-enabled/50-mod-http-passenger.conf ; fi
+sudo ls /etc/nginx/conf.d/mod-http-passenger.conf
