5a7cbe3810fea0aebfd574b3903c79ba7095a70a
duffyjp <duffy.jp@gmail.com>
2018-07-19 21:14:46 +0000

    fixed ruby's ssl support thru rvm

--- 40_setup_rvm.sh
+++ 40_setup_rvm.sh
@@ -11,3 +11,11 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 adduser --disabled-password deploy 2> /dev/null
 usermod -a -G rvm deploy
 
+gem install bundler
+
+su - deploy -c "rvm autolibs disable"
+
+# This is broken in 18.04 I guess.
+# See https://github.com/rvm/rvm/issues/4357
+apt purge libssl-dev && apt install libssl1.0-dev
+
